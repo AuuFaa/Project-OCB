000526a65f27db7f38ef2f422f81c114b3ba8ee7
iji <80830735+AuuFaa@users.noreply.github.com>
2023-12-01 00:01:35 +0800

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Utama from "./pages/Utama";
+import Aturcara from "./pages/Aturcara";
+import Lokasi from "./pages/Lokasi";
+import Rsvp from "./pages/Rsvp";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Utama />} />
+        <Route path="/aturcara" element={<Aturcara />} />
+        <Route path="/lokasi" element={<Lokasi />} />
+        <Route path="/rsvp" element={<Rsvp />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
