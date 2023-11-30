3c0b6bc01f53d8a2c09e89f790229ef19b607a44
iji <80830735+AuuFaa@users.noreply.github.com>
2023-12-01 01:09:00 +0800

    create user model

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,25 @@
-import express from 'express';
+import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
-
-
-
+import mongoose from "mongoose";
 
 const app = express();
 
-app.get('/',(request, response)=>{
-    console.log(request);
-    return response.status(608).send('Welcome to my domain bruh');
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(608).send("Welcome to my domain bruh");
 });
 
 // //GET DATABASE_URL variable from the .env file
 // const uri = process.env.MONGO;
 
-mongoose.connect(mongoDBURL)
-.then(()=>{
-        console.log('App connected to database');
-        app.listen(PORT, () =>{
-            console.log(`App is listening to port: ${PORT}`);
-        });
-    }).catch((error)=>{
-        console.log(error);
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
