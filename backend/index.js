369b509ee5141b701721c07e586ef998d738bed1
iji <80830735+AuuFaa@users.noreply.github.com>
2023-12-01 22:20:27 +0800

    create api signup

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,17 @@ import express, { request, response } from 'express';
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 
 const app = express();
 
+app.use(express.json());
+
 
 
 app.use("/api/user", userRouter);
+app.use('/api/auth', authRouter);
 // //GET DATABASE_URL variable from the .env file
 // const uri = process.env.MONGO;
 
