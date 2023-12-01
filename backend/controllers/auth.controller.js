7b0fc9bdb2d79aa09fc4bbedf8347d9be867e648
iji <80830735+AuuFaa@users.noreply.github.com>
2023-12-01 22:37:15 +0800

    create a middleware and error

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import user from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new user({ username, email, password: hashedPassword });
@@ -9,6 +10,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(203).json("User created successfully!");
   } catch (error) {
-    res.status(501).json(error.message);
+    next(error);
   }
 };
